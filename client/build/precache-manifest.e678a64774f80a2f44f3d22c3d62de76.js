15826c26297b252008443fa3a04b301725c42d7e
Andrea Bentley <andrea@bentleytech.com>
2020-09-29 22:24:27 -0400

    ran build, updated package.json node version

---
+++ client/build/precache-manifest.e678a64774f80a2f44f3d22c3d62de76.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ab88d88c33b219ff15b47f2aae1467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87128fb94f80c12e0cb6",
+    "url": "/static/css/2.daa9285a.chunk.css"
+  },
+  {
+    "revision": "a640d5291c0820f9d33d",
+    "url": "/static/css/main.c9e167ae.chunk.css"
+  },
+  {
+    "revision": "87128fb94f80c12e0cb6",
+    "url": "/static/js/2.e2f9bb30.chunk.js"
+  },
+  {
+    "revision": "82a83e7bc5c9ec526cf715c8c478fe72",
+    "url": "/static/js/2.e2f9bb30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a640d5291c0820f9d33d",
+    "url": "/static/js/main.ebd63bce.chunk.js"
+  },
+  {
+    "revision": "c094669b6c7818d0923b",
+    "url": "/static/js/runtime-main.cbf9cff0.js"
+  },
+  {
+    "revision": "2a577ff421ea3673dec5efe059e99d58",
+    "url": "/static/media/ac512x512.2a577ff4.png"
+  },
+  {
+    "revision": "b24a016b81ef902a6c0187a7a4341146",
+    "url": "/static/media/android-chrome-192x192.b24a016b.png"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
